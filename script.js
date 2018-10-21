876ec9a7951f04e5f8d8fd8fcc2be5c2d356de25
AntonDolgov <dlgvnt@gmail.com>
2018-10-21 15:12:42 +0300

    json load fix

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /* Shapes */
+
 var svgContainer = document.getElementById('svgContainer');
 var animItem = bodymovin.loadAnimation({
     wrapper: svgContainer,
     animType: 'svg',
     loop: true,
-    path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
+    path: 'data.json'
 });
